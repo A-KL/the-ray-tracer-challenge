0798fb5d347ebde94dcea712bb83e00964eb8ec0
Anatolii Klots <anatolii.klots@knab.nl>
2020-02-28 21:48:36 +0100

    color added

--- src/renderer/Color.h
+++ src/renderer/Color.h
@@ -1,26 +1,18 @@
 #pragma once
+
 class Color
 {
 public:
-	Color(int); 
-
-	int R();
-
-	int G();
-
-	int B();
-
-	int A();
-
-	static Color Green();
-
 	static const int Bpp24 = 24;
 
 	static const int Bpp32 = 32;
 
 	static const int Bpp8 = 8;
 
-private:
-	char* _data;
-};
+	static const int Bpp1 = 1;
 
+	virtual int Bpp();
+
+protected:
+	virtual const char& GetBuffer() = 0;
+};
---
+++ src/renderer/ColorRgba.cpp
@@ -0,0 +1,3 @@
+#include "ColorRgba.h"
+
+
