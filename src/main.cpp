26b8c64e6dbf3de7c7cb38428c612aede20d035d
Anatolii Klots <anatolii.klots@knab.nl>
2020-07-12 23:24:58 +0200

    tests were separated

--- src/main.cpp
+++ src/main.cpp
@@ -16,84 +16,15 @@
 #include "ray-tracer/Projectile.h"
 #include "ray-tracer/Matrix.h"
 
+#include "tests.h"
+
 using namespace std;
-
-void test_matrix_translation()
-{
-	Matrix<int, 4, 4> transform;
-
-	matrix_identity(transform);
-	matrix_translate(5, -3, 2, transform);
-
-	int point[4] = { -3, 4, 5, 1 };
-	int result[4];
-
-	matrix_mul(transform, point, result);
-
-	assert(2 == result[0]);
-	assert(1 == result[1]);
-	assert(7 == result[2]);
-}
-
-void run_tests()
-{
-	// Set up
-
-	Matrix<int, 3, 3> m0 = {
-		1,2,3,
-		4,5,6,
-		7,8,9
-	};
-
-	Matrix<int, 3, 3> m1 = {
-		1,2,3,
-		4,5,6,
-		7,8,9
-	};
-
-	Matrix<int, 3, 3> identity;
-	matrix_identity(identity);
-
-	Matrix<int, 3, 3> mux;
-	matrix_mul(m0, m1, mux);
-
-	Matrix<int, 3, 3> mux2;
-	matrix_mul(mux, identity, mux2);
-
-	Matrix<int, 2, 2> sub;
-	matrix_sub(m0, sub);
-
-	// Assert
-
-	assert(m0 == m1);
-
-	assert(mux == mux);
-
-	assert(1 == sub.Data[0][0]);
-	assert(2 == sub.Data[0][1]);
-	assert(4 == sub.Data[1][0]);
-	assert(5 == sub.Data[1][1]);
-
-
-	int tuple[3] = { 2, 2, 2 };
-
-	int tuple_mul[3];
-
-	matrix_mul(m0, tuple, tuple_mul);
-
-	assert(24 == tuple_mul[0]);
-	assert(30 == tuple_mul[1]);
-	assert(36 == tuple_mul[2]);
-
-	// int d0 = sub.Determinant();
-	// int d = sub.Minor(2,2);
-
-	test_matrix_translation();
-}
 
 int main()
 {
+#ifdef _DEBUG
 	run_tests();
+#endif
 
 	Point3D start(0, 1, 0);
 	Vector3D velocity(1, 1.8, 0);
