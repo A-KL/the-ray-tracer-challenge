d52f42d7737fe34e69cbc3b50b2f7c791ef672a9
Anatolii Klots <anatolii.klots@knab.nl>
2020-09-14 22:41:47 +0200

    extracting Matrix implementation into cpp file

--- src/ray-tracer/Matrix.h
+++ src/ray-tracer/Matrix.h
@@ -5,94 +5,25 @@ struct Matrix
 {
 	TItem Data[TSizeX][TSizeY];
 
-	inline const TItem& operator[](int Index) const
-	{
-		return Data[Index];
-	}
+	inline const TItem& operator[](int Index) const;
 
-	inline Matrix<TItem, TSizeX, TSizeY>& operator+=(const Matrix<TItem, TSizeX, TSizeY>& other)
-	{
-		for (int i = 0; i < TSizeX * TSizeY; i++)
-		{
-			this->Data[i] += other.Data[i];
-		}
-		return *this;
-	}
+	Matrix<TItem, TSizeX, TSizeY>& operator+=(const Matrix<TItem, TSizeX, TSizeY>& other);
 
-	inline Matrix<TItem, TSizeX, TSizeY>& operator-=(const Matrix<TItem, TSizeX, TSizeY>& other)
-	{
-		for (int i = 0; i < TSizeX * TSizeY; i++)
-		{
-			this->Data[i][0] -= other.Data[i][0];
-		}
-		return *this;
-	}
+	Matrix<TItem, TSizeX, TSizeY>& operator-=(const Matrix<TItem, TSizeX, TSizeY>& other);
 
-	inline bool operator==(const Matrix<TItem, TSizeX, TSizeY>& other)
-	{
-		int x;
+	bool operator==(const Matrix<TItem, TSizeX, TSizeY>& other);
 
-		TItem* item0 = &Data[0][0];
-		const TItem* item1 = &other.Data[0][0];
+	bool operator!=(const Matrix<TItem, TSizeX, TSizeY>& other);
 
-		for (x = 0; x < TSizeX * TSizeY; x++)
-		{
-			if (item1[x] != item1[x])
-			{
-				return false;
-			}
+	inline const TItem Determinant();
 
-		}
+	inline const TItem Minor(int row, int col);
 
-		return true;
-	}
-
-	inline bool  operator!=(const Matrix<TItem, TSizeX, TSizeY>& other)
-	{
-		return !(other == this);
-	}
-
-	inline const TItem Determinant()
-	{
-		TItem determinant = 0;
-
-		if (TSizeX == 2 && TSizeY == 2)
-		{
-			determinant = Data[0][0] * Data[1][1] - Data[0][1] * Data[1][0];
-		}
-		else
-		{
-			for (int col = 0; col < (TSizeY - 1); ++col)
-			{
-				TItem d = Data[0][col];
-
-				determinant += d;// *Cofactor(0, col);
-			}
-		}
-
-		return determinant;
-	}
-
-	inline const TItem Minor(int row, int col)
-	{
-		Matrix<TItem, TSizeX - 1, TSizeY - 1> result;
-
-		matrix_remove(*this, row, col, result);
-
-		return result.Determinant();
-	}
-
-	inline const TItem Cofactor(int row, int col)
-	{
-		int minor = Minor(row, col);
-
-		// if odd return -minor otherwise minor
-		return ((row + col) % 2) ? -minor : minor;
-	}
+	inline const TItem Cofactor(int row, int col);
 };
 
 template<class TItem>
-struct Matrix2 : Matrix<TItem, 2, 2>
+struct Matrix2 :  Matrix<TItem, 2, 2>
 { };
 
 template<class TItem>
@@ -102,142 +33,3 @@ struct Matrix3 : Matrix<TItem, 3, 3>
 template<class TItem>
 struct Matrix4 : Matrix<TItem, 4, 4>
 { };
-
-template<class TItem, int TSizeX, int TSizeY>
-void matrix_remove(
-	const Matrix<TItem, TSizeX, TSizeY>& matrix,
-	int row, int col,
-	Matrix<TItem, TSizeX - 1, TSizeX - 1>& result)
-{
-	int x, y, x1, y1;
-
-	for (x = 0, x1 = 0; x < TSizeX; x++)
-	{
-		if (x == row)
-			continue;
-
-		for (y = 0, y1 = 0; y < TSizeY; y++)
-		{
-			if (y == col)
-				continue;
-
-			result.Data[x1][y1] = matrix.Data[x][y];
-			y1++;
-		}
-		x1++;
-	}
-}
-
-template<class TItem, int TSizeX, int TSizeY>
-void matrix_identity(
-	Matrix<TItem, TSizeX, TSizeY>& matrix)
-{
-	int x, y;
-
-	for (x = 0; x < TSizeX; x++)
-	{
-		for (y = 0; y < TSizeY; y++)
-		{
-			matrix.Data[x][y] = (x == y);
-		}
-	}
-}
-
-template<class TItem, int TSizeX, int TSizeY>
-void matrix_inverse(
-	Matrix<TItem, TSizeX, TSizeY>& matrix)
-{
-	//???
-}
-
-template<class TItem, int TSizeX, int TSizeY>
-void matrix_mul(
-	const Matrix<TItem, TSizeX, TSizeY>& matrix1,
-	const Matrix<TItem, TSizeX, TSizeY>& matrix2,
-	Matrix<TItem, TSizeX, TSizeY>& result)
-{
-	int x, y, j;
-
-	for (x = 0; x < TSizeX; x++)
-	{
-		for (y = 0; y < TSizeY; y++)
-		{
-			result.Data[x][y] = 0;
-
-			for (j = 0; j < TSizeY; j++)
-			{
-				result.Data[x][y] += matrix1.Data[x][j] * matrix2.Data[j][y];
-			}
-		}
-	}
-}
-
-template<class TItem>
-void matrix_mul(
-	const Matrix<TItem, 3, 3>& matrix,
-	const Vector3D& other,
-	Vector3D& result)
-{
-	result.X = matrix.Data[0][0] * other.X + matrix.Data[1][0] * other.Y + matrix.Data[2][0] * other.Z;
-	result.Y = matrix.Data[0][1] * other.X + matrix.Data[1][1] * other.Y + matrix.Data[2][1] * other.Z;
-	result.Z = matrix.Data[0][2] * other.X + matrix.Data[1][2] * other.Y + matrix.Data[2][2] * other.Z;
-}
-
-template<class TItem, int TSize>
-void matrix_mul(
-	const Matrix<TItem, TSize, TSize>& matrix,
-	const TItem other[],
-	TItem result[])
-{
-	int x, y, index;
-
-	for (index = 0; index < TSize; index++)
-	{
-		result[index] = 0;
-
-		for (x = 0; x < TSize; x++)
-		{
-			result[index] += (other[x] * matrix.Data[x][index]);
-		}
-	}
-}
-
-template<class TItem, int TSizeX, int TSizeY>
-void matrix_transpose(
-	const Matrix<TItem, TSizeX, TSizeY>& matrix,
-	Matrix<TItem, TSizeX, TSizeY>& result)
-{
-	int x, y;
-
-	for (x = 0; x < TSizeX; x++)
-	{
-		for (y = 0; y < TSizeY; y++)
-		{
-			result.Data[y][x] = matrix.Data[y][x];
-		}
-	}
-}
-
-template<class TItem>
-void matrix_translate(
-	TItem x,
-	TItem y,
-	TItem z,
-	Matrix<TItem, 4, 4>& result)
-{
-	result.Data[3][0] = x;
-	result.Data[3][1] = y;
-	result.Data[3][2] = z;
-}
-
-template<class TItem>
-void matrix_scaling(
-	TItem x,
-	TItem y,
-	TItem z,
-	Matrix<TItem, 4, 4>& result)
-{
-	result.Data[3][0] = x;
-	result.Data[3][1] = y;
-	result.Data[3][2] = z;
-}
--- src/tests.cpp
+++ src/tests.cpp
@@ -4,15 +4,154 @@
 #include <list>
 #include <cassert>
 
-#include <SDL.h>
-
-#include "renderer/Color.h"
-#include "renderer/SDLRenderer.h"
-
 #include "ray-tracer/Vector3D.h"
 #include "ray-tracer/Point3D.h"
-#include "ray-tracer/Projectile.h"
+
 #include "ray-tracer/Matrix.h"
+#include "ray-tracer/Matrix.cpp"
+
+//#include "ray-tracer/matrix_basic.h"
+
+#pragma once
+
+template<class TItem, int TSizeX, int TSizeY>
+void matrix_remove(
+	const Matrix<TItem, TSizeX, TSizeY>& matrix,
+	int row, int col,
+	Matrix<TItem, TSizeX - 1, TSizeX - 1>& result)
+{
+	int x, y, x1, y1;
+
+	for (x = 0, x1 = 0; x < TSizeX; x++)
+	{
+		if (x == row)
+			continue;
+
+		for (y = 0, y1 = 0; y < TSizeY; y++)
+		{
+			if (y == col)
+				continue;
+
+			result.Data[x1][y1] = matrix.Data[x][y];
+			y1++;
+		}
+		x1++;
+	}
+}
+
+template<class TItem, int TSizeX, int TSizeY>
+void matrix_identity(
+	Matrix<TItem, TSizeX, TSizeY>& matrix)
+{
+	int x, y;
+
+	for (x = 0; x < TSizeX; x++)
+	{
+		for (y = 0; y < TSizeY; y++)
+		{
+			matrix.Data[x][y] = (x == y);
+		}
+	}
+}
+
+template<class TItem, int TSizeX, int TSizeY>
+void matrix_inverse(
+	Matrix<TItem, TSizeX, TSizeY>& matrix)
+{
+	//???
+}
+
+template<class TItem, int TSizeX, int TSizeY>
+void matrix_mul(
+	const Matrix<TItem, TSizeX, TSizeY>& matrix1,
+	const Matrix<TItem, TSizeX, TSizeY>& matrix2,
+	Matrix<TItem, TSizeX, TSizeY>& result)
+{
+	int x, y, j;
+
+	for (x = 0; x < TSizeX; x++)
+	{
+		for (y = 0; y < TSizeY; y++)
+		{
+			result.Data[x][y] = 0;
+
+			for (j = 0; j < TSizeY; j++)
+			{
+				result.Data[x][y] += matrix1.Data[x][j] * matrix2.Data[j][y];
+			}
+		}
+	}
+}
+
+template<class TItem>
+void matrix_mul(
+	const Matrix<TItem, 3, 3>& matrix,
+	const Vector3D& other,
+	Vector3D& result)
+{
+	result.X = matrix.Data[0][0] * other.X + matrix.Data[1][0] * other.Y + matrix.Data[2][0] * other.Z;
+	result.Y = matrix.Data[0][1] * other.X + matrix.Data[1][1] * other.Y + matrix.Data[2][1] * other.Z;
+	result.Z = matrix.Data[0][2] * other.X + matrix.Data[1][2] * other.Y + matrix.Data[2][2] * other.Z;
+}
+
+template<class TItem, int TSize>
+void matrix_mul(
+	const Matrix<TItem, TSize, TSize>& matrix,
+	const TItem other[],
+	TItem result[])
+{
+	int x, index;
+
+	for (index = 0; index < TSize; index++)
+	{
+		result[index] = 0;
+
+		for (x = 0; x < TSize; x++)
+		{
+			result[index] += (other[x] * matrix.Data[x][index]);
+		}
+	}
+}
+
+template<class TItem, int TSizeX, int TSizeY>
+void matrix_transpose(
+	const Matrix<TItem, TSizeX, TSizeY>& matrix,
+	Matrix<TItem, TSizeX, TSizeY>& result)
+{
+	int x, y;
+
+	for (x = 0; x < TSizeX; x++)
+	{
+		for (y = 0; y < TSizeY; y++)
+		{
+			result.Data[y][x] = matrix.Data[y][x];
+		}
+	}
+}
+
+template<class TItem>
+void matrix_translate(
+	TItem x,
+	TItem y,
+	TItem z,
+	Matrix<TItem, 4, 4>& result)
+{
+	result.Data[3][0] = x;
+	result.Data[3][1] = y;
+	result.Data[3][2] = z;
+}
+
+template<class TItem>
+void matrix_scaling(
+	TItem x,
+	TItem y,
+	TItem z,
+	Matrix<TItem, 4, 4>& result)
+{
+	result.Data[3][0] = x;
+	result.Data[3][1] = y;
+	result.Data[3][2] = z;
+}
 
 using namespace std;
 
