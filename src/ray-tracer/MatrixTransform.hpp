95cb6da27b854805e2bd0a7ecd16fe3814444a04
Anatolii Klots <anatolii.klots@gmail.com>
2022-01-08 19:47:28 +0100

    rotations

--- src/ray-tracer/MatrixTransform.hpp
+++ src/ray-tracer/MatrixTransform.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <math.h> 
+
 #include "Matrix.h"
 #include "MatrixBase.hpp"
+
+#define PI 3.14159265
 
 template<class TItem>
 void matrix_translate(
@@ -29,4 +33,43 @@ void matrix_scale(
 	result.Data[0][0] = x;
 	result.Data[1][1] = y;
 	result.Data[2][2] = z;
+}
+
+template<class TItem>
+void matrix_rotate_x(
+	float r,
+	Matrix<TItem, 4, 4>& result)
+{
+	matrix_identity(result);
+	//r * PI / 180
+	result.Data[1][1] = cos(r);
+	result.Data[2][1] = -sin(r);
+	result.Data[1][2] = sin(r);
+	result.Data[2][2] = cos(r);
+}
+
+template<class TItem>
+void matrix_rotate_y(
+	float r,
+	Matrix<TItem, 4, 4>& result)
+{
+	matrix_identity(result);
+
+	result.Data[0][0] = cos(r);
+	result.Data[2][0] = sin(r);
+	result.Data[0][2] =-sin(r);
+	result.Data[2][2] = cos(r);
+}
+
+template<class TItem>
+void matrix_rotate_z(
+	float r,
+	Matrix<TItem, 4, 4>& result)
+{
+	matrix_identity(result);
+
+	result.Data[0][0] = cos(r);
+	result.Data[1][0] =-sin(r);
+	result.Data[0][2] = sin(r);
+	result.Data[1][1] = cos(r);
 }
