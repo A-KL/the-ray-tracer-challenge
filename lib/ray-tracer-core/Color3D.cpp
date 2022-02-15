396f8b62a500a7d15998e4cf9f97d02bef24f46b
Anatolii Klots <anatolii.klots@SkyKick.com>
2022-02-15 16:37:43 +0100

    more fixes

--- lib/ray-tracer-core/Color3D.cpp
+++ lib/ray-tracer-core/Color3D.cpp
@@ -5,3 +5,44 @@
 const Color3D Color3D::Black(0, 0, 0);
 
 const Color3D Color3D::White(1, 1, 1);
+
+const Color3D Color3D::operator*(const Color3D& other) const
+{
+	return Color3D{ R * other.R, G * other.G, B * other.B };
+}
+
+const Color3D Color3D::operator+(const Color3D& other) const
+{
+	return Color3D{ R + other.R, G + other.G, B + other.B };
+}
+
+const Color3D Color3D::operator*(double other) const
+{
+	return Color3D{ R * other, G * other, B * other };
+}
+
+bool Color3D::operator==(const Color3D& other) const
+{
+	return
+		Mathf::Approximately(R, other.R) &&
+		Mathf::Approximately(G, other.G) &&
+		Mathf::Approximately(B, other.B);
+}
+
+Color3D::operator unsigned int() const
+{
+	unsigned char r = (R > 1 ? 1 : R < 0 ? 0 : R) * 255;
+	unsigned char g = (G > 1 ? 1 : G < 0 ? 0 : G) * 255;
+	unsigned char b = (B > 1 ? 1 : B < 0 ? 0 : B) * 255;
+
+	return (r << 16) | (g << 8) | b;
+}
+
+Color3D::operator unsigned short() const
+{
+	unsigned char r = (R > 1 ? 1 : R < 0 ? 0 : R) * 255;
+	unsigned char g = (G > 1 ? 1 : G < 0 ? 0 : G) * 255;
+	unsigned char b = (B > 1 ? 1 : B < 0 ? 0 : B) * 255;
+
+	return ((r >> 3) << 11 | (g >> 2) << 5 | b >> 3);
+}
--- lib/ray-tracer-core/Color3D.h
+++ lib/ray-tracer-core/Color3D.h
@@ -3,8 +3,6 @@
 #include <iostream>
 #include <cassert>
 
-#include "Mathf.h"
-
 struct Color3D
 {
 	Color3D(double r = 1, double g = 1, double b = 1)
@@ -27,46 +25,17 @@ struct Color3D
 		}
 	}
 
-	const Color3D operator*(const Color3D& other) const
-	{
-		return Color3D { R * other.R, G * other.G, B * other.B };
-	}
+	const Color3D operator*(const Color3D& other) const;
 
-	const Color3D operator+(const Color3D& other) const
-	{
-		return Color3D { R + other.R, G + other.G, B + other.B };
-	}
+	const Color3D operator+(const Color3D& other) const;
 
-	const Color3D operator*(double other) const
-	{
-		return Color3D { R * other, G * other, B * other };
-	}
+	const Color3D operator*(double other) const;
 
-	bool operator==(const Color3D& other) const
-	{
-		return
-			Mathf::Approximately(R, other.R) &&
-			Mathf::Approximately(G, other.G) &&
-			Mathf::Approximately(B, other.B);
-	}
+	bool operator==(const Color3D& other) const;
 
-	inline operator unsigned int() const
-	{
-		unsigned char r = (R > 1 ? 1 : R < 0 ? 0 : R) * 255;
-		unsigned char g = (G > 1 ? 1 : G < 0 ? 0 : G) * 255;
-		unsigned char b = (B > 1 ? 1 : B < 0 ? 0 : B) * 255;
+	operator unsigned int() const;
 
-		return (r << 16) | (g << 8) | b;
-	}
-
-	inline operator unsigned short() const
-	{
-		unsigned char r = (R > 1 ? 1 : R < 0 ? 0 : R) * 255;
-		unsigned char g = (G > 1 ? 1 : G < 0 ? 0 : G) * 255;
-		unsigned char b = (B > 1 ? 1 : B < 0 ? 0 : B) * 255;
-
-		return ((r >> 3) << 11 | (g >> 2) << 5 | b >> 3);
-	}
+	operator unsigned short() const;
 
 	friend std::ostream& operator<<(std::ostream& out, const Color3D& color)
 	{
