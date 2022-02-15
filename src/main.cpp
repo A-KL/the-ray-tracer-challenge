32796eebe4398ce891ad80bd2dd7d3e89aeb4441
Anatolii Klots <anatolii.klots@gmail.com>
2022-02-16 00:29:49 +0100

    Yes!

--- src/main.cpp
+++ src/main.cpp
@@ -142,7 +142,7 @@ void run_light_demo(Canvas& canvas)
 	const double half = wall_size / 2;
 
 	Sphere3D sphere(Point3D(0, 0, 0), Matrix4d::Scale(1, 1, 1), Material(1, 0.2, 1), 1);
-	Light3D light(Point3D(-10, -10, -10), Color3D::White);
+	Light3D light(Point3D(-10, 10, -10), Color3D::White);
 
 	Point3D ray_origin(0, 0, -5);
 
