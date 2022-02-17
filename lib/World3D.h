70b21c718f5ad0b74a6ffad4dbbcad124b4aa37c
Anatolii Klots <anatolii.klots@SkyKick.com>
2022-02-17 12:34:42 +0100

    adding ray tracing for multiple shapes

--- lib/World3D.h
+++ lib/World3D.h
@@ -7,4 +7,6 @@ public:
 	std::list<Light3D> Lights;
 
 	std::list<Shape3D> Shapes;
+
+	//Color3D Intersect(const Ray3D& ray) const;
 };
--- src/tests_world.cpp
+++ src/tests_world.cpp
@@ -12,14 +12,18 @@
 #include "../lib/MatrixOps.hpp"
 #include "../lib/MatrixTransform.hpp"
 
-#include "../lib/Color3D.h"
 #include "../lib/Sphere3D.h"
 #include "../lib/Light3D.h"
+
+#include "../lib/Ray3D.h"
+#include "../lib/Intersection.h"
+#include "../lib/RayTracer.h"
+
 #include "../lib/World3D.h"
 
 #include "tests.h"
 
-void run_world_tests()
+void test_world_default()
 {
 	// Setup
 	World3D world;
@@ -36,4 +40,40 @@ void run_world_tests()
 	// Assert
 	assert(1 == world.Lights.size());
 	assert(2 == world.Shapes.size());
+}
+
+void test_world_intersection()
+{
+	// Setup
+	World3D world;
+	Light3D light(Point3D(-10, 10, -10), Color3D(1, 1, 1));
+
+	Sphere3D sphere1(Material(Color3D(0.8, 1.0, 0.6), 1, 0.7, 0.2));
+	Sphere3D sphere2(Matrix4d::Scale(0.5, 0.5, 0.5));
+
+	Ray3D ray(Point3D(0, 0, -5), Vector3D(0, 0, 1));
+
+	world.Lights.push_back(light);
+	world.Shapes.push_back(sphere1);
+	world.Shapes.push_back(sphere2);
+
+	// Act
+	auto intersections = ray_intersect(world.Shapes, ray);
+
+	// Assert
+	assert(4 == intersections.size());
+
+	auto iter = intersections.begin();
+
+	assert(4 == (iter++)->T());
+	assert(4.5 == (iter++)->T());
+	assert(5.5 == (iter++)->T());
+	assert(6 == (iter)->T());
+}
+
+void run_world_tests()
+{
+	test_world_default();
+
+	test_world_intersection();
 }
