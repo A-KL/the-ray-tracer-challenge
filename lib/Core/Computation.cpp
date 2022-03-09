449ed96b4d10269de7d36ae0478f0ed5d1557406
Anatolii Klots <anatolii.klots@gmail.com>
2022-03-09 16:47:33 +0100

    test fixed!

--- lib/Core/Computation.cpp
+++ lib/Core/Computation.cpp
@@ -24,7 +24,8 @@ const Computation Computation::Prepare(const Intersection& intersection, const R
 	auto normal = intersection.Shape->NormalAt(position);
 	auto camera = -ray.Direction;
 	auto inside = Vector3D::Dot(normal, camera) < 0;
+	normal = inside ? -normal : normal;
 	auto over_point = position + normal * DBL_EPSILON;
 
-	return Computation{ intersection, position, over_point, inside ? -normal : normal, camera, inside };
+	return Computation{ intersection, position, over_point, normal, camera, inside };
 }
