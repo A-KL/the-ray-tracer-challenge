0a9d7664b08c303b795fbfcdc2873115d793635d
Anatolii Klots <anatolii.klots@gmail.com>
2022-06-30 00:39:59 +0200

    adding RingColor3D

--- src-sdl/examples.cpp
+++ src-sdl/examples.cpp
@@ -278,3 +278,57 @@ void run_scene_plane_demo(Canvas& canvas)
 
 	main_camera.Render(scene, canvas);
 }
+
+void run_scene_patterns_demo(Canvas& canvas)
+{
+	const int w = canvas.Witdth(); //100
+	const int h = canvas.Height(); //50
+
+	auto left_wall_location =
+		Matrix4d::Translate(0, 0, 5) *
+		Matrix4d::RotateY(-M_PI / 4) * Matrix4d::RotateX(M_PI / 2) *
+		Matrix4d::Scale(10, 0.01, 10);
+
+
+	auto right_wall_location =
+		Matrix4d::Translate(0, 0, 5) *
+		Matrix4d::RotateY(M_PI / 4) * Matrix4d::RotateX(M_PI / 2) *
+		Matrix4d::Scale(10, 0.01, 10);
+
+	Material3D floor_material(GradientColor3D(Color3D::Green, Color3D::Red), 0.1, 0.9, 0);
+	Material3D left_wall_material(GradientColor3D(Color3D::Green, Color3D::Red), 0.1, 0.9, 0);
+	Material3D right_wall_material(GradientColor3D(Color3D::Blue, Color3D::White), 0.1, 0.9, 0);
+
+	Plane3D floor(Matrix4d::Scale(10, 0.01, 10), floor_material);
+	Sphere3D left_wall(left_wall_location, left_wall_material);
+	Sphere3D right_wall(right_wall_location, right_wall_material);
+	// -----------------------------------------------------------------------------
+
+	Sphere3D middle(Matrix4d::Translate(-0.5, 1, 0.5), Material3D(SolidColor3D(0.1, 1, 0.5), 0.1, 0.7, 0.3));
+
+	Sphere3D right(Matrix4d::Translate(1.5, 0.5, -0.5) * Matrix4d::Scale(0.5), Material3D(SolidColor3D(0.5, 1, 0.1), 0.1, 0.7, 0.3));
+
+	Sphere3D left(Matrix4d::Translate(-1.5, 0.33, -0.75) * Matrix4d::Scale(0.33), Material3D(SolidColor3D(1, 0.8, 0.1), 0.1, 0.7, 0.3));
+
+	// -----------------------------------------------------------------------------
+
+	Light3D main_light(Point3D(-10, 10, -10), Color3D(1, 1, 1));
+
+	Camera main_camera(w, h, M_PI / 3, Point3D(0, 1.5, -5), Point3D(0, 1, 0), Vector3D(0, 1, 0));
+
+	// -----------------------------------------------------------------------------
+
+	Scene3D scene;
+
+	scene.Lights.push_back(&main_light);
+
+	scene.Shapes.push_back(&floor);
+	scene.Shapes.push_back(&left_wall);
+	scene.Shapes.push_back(&right_wall);
+
+	scene.Shapes.push_back(&middle);
+	scene.Shapes.push_back(&right);
+	scene.Shapes.push_back(&left);
+
+	main_camera.Render(scene, canvas);
+}
