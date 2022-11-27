bd711ff1394df03a55e820bfa876f313b8769ecd
Anatolii <anatolii.klots@gmail.com>
2022-11-27 16:21:01 +0100

    adding new properties

--- lib/Core/Material3D.cpp
+++ lib/Core/Material3D.cpp
@@ -3,18 +3,18 @@
 #include <math.h>
 #include "Mathf.h"
 
-const Material3D Material3D::Default(SolidColor3D::Default, 0.1, 0.9, 0.9, 200, 0);
+const Material3D Material3D::Default(SolidColor3D::Default, 0.1, 0.9, 0.9, 200, 0, 0, 1.0);
 
 Material3D::Material3D(const Material3D& material)
 	: Material3D(*material.Pattern, material.Ambient, material.Diffuse, material.Specular, material.Shininess, material.Reflective)
 { }
 
-Material3D::Material3D(const ColorPattern& pattern, double ambient, double diffuse, double specular, double shininess, double reflective)
-	: Material3D(&pattern, ambient, diffuse, specular, shininess, reflective)
+Material3D::Material3D(const ColorPattern& pattern, double ambient, double diffuse, double specular, double shininess, double reflective, double transparency, double refractive_index)
+	: Material3D(&pattern, ambient, diffuse, specular, shininess, reflective, transparency, refractive_index)
 {  }
 
-Material3D::Material3D(const ColorPattern* pattern, double ambient, double diffuse, double specular, double shininess, double reflective)
-	: Pattern(pattern), Ambient(ambient), Diffuse(diffuse), Specular(specular), Shininess(shininess), Reflective(reflective)
+Material3D::Material3D(const ColorPattern* pattern, double ambient, double diffuse, double specular, double shininess, double reflective, double transparency, double refractive_index)
+	: Pattern(pattern), Ambient(ambient), Diffuse(diffuse), Specular(specular), Shininess(shininess), Reflective(reflective), Transparency(transparency), RefractiveIndex(refractive_index)
 {  }
 
 bool Material3D::operator==(const Material3D& other) const
@@ -25,5 +25,7 @@ bool Material3D::operator==(const Material3D& other) const
 		Mathf<double>::Approximately(Diffuse, other.Diffuse) &&
 		Mathf<double>::Approximately(Specular, other.Specular) &&
 		Mathf<double>::Approximately(Shininess, other.Shininess) &&
-		Mathf<double>::Approximately(Reflective, other.Reflective);
+		Mathf<double>::Approximately(Reflective, other.Reflective) &&
+		Mathf<double>::Approximately(Transparency, other.Transparency) &&
+		Mathf<double>::Approximately(RefractiveIndex, other.RefractiveIndex);
 }
--- lib/Core/Material3D.h
+++ lib/Core/Material3D.h
@@ -6,9 +6,9 @@ struct Material3D
 {
 	Material3D(const Material3D& material);
 
-	Material3D(const ColorPattern& pattern, double ambient = 0.1, double diffuse = 0.9, double specular = 0.9, double shininess = 200, double reflective = 0.0);
+	Material3D(const ColorPattern& pattern, double ambient = 0.1, double diffuse = 0.9, double specular = 0.9, double shininess = 200, double reflective = 0.0, double transparency = 0, double refractive_index = 1.0);
 
-	Material3D(const ColorPattern* pattern, double ambient = 0.1, double diffuse = 0.9, double specular = 0.9, double shininess = 200, double reflective = 0.0);
+	Material3D(const ColorPattern* pattern, double ambient = 0.1, double diffuse = 0.9, double specular = 0.9, double shininess = 200, double reflective = 0.0, double transparency = 0, double refractive_index = 1.0);
 
 	bool operator==(const Material3D& other) const;
 
@@ -19,6 +19,8 @@ struct Material3D
 	const double Specular;
 	const double Shininess;
 	const double Reflective;
+	const double Transparency;
+	const double RefractiveIndex;
 
 	const static Material3D Default;
 };
